0f0a3250688e3acf06e5146e0f37bc5963609e49
MattiaGuazzini07 <145323347+MattiaGuazzini07@users.noreply.github.com>
2025-03-20 08:43:51 +0100

    add initial components and templates for chef, menu, login, staff, home-page, reservations, and administrator

---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.sass'
+})
+export class HomePageComponent {
+
+}
